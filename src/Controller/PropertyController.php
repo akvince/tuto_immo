cde8311d92234108e1a6e70ab2ec77e3459fe189
vdeflandre <deflandre.vincent@gmail.com>
2020-04-21 14:11:18 +0200

    feat: contact

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,10 +2,14 @@
     namespace App\Controller;
 
     use App\Entity\Property;
+    use App\Entity\Contact;
     use App\Repository\PropertyRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
     use Doctrine\ORM\EntityManagerInterface;
+    use App\Form\ContactType;
+    use App\Notification\ContactNotification;
 
     class PropertyController extends AbstractController
     {
@@ -17,16 +21,33 @@
         }
 
 
-        public function Index(Property $property, string $slug):response
+        public function Index(Property $property, string $slug, Request $request, ContactNotification $notification): Response
         {
+            
             if($property->getSlug() !== $slug){
                 return $this->redirectToRoute('property',[
                     'id' => $property->getId(),
                     'slug' => $property->getSlug()
                 ], 301);
             }
+            
+            $contact = new Contact();
+            $contact->setProperty($property);
+            $form = $this->createForm(ContactType::class, $contact);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $notification->notify($contact);
+                $this->addFlash('success', 'Votre email a bien été envoyé');
+                return $this->redirectToRoute('property',[
+                    'id' => $property->getId(),
+                    'slug' => $property->getSlug()
+                ]);
+            }
+
             return $this->render('property/property.html.twig', [
-                'property' => $property
+                'property' => $property,
+                'form' => $form->createView()
             ]);
         }
     }
